751de28e647d398f065edbd1168504fba55104df
Elliot Sanchez <github@elliotsanchez.com>
2016-11-15 10:40:08 -0600

    comments out non-functional methods and creates working proof of concept for pulling source code of page

--- weather-outlook/ViewController.swift
+++ weather-outlook/ViewController.swift
@@ -10,11 +10,99 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = URL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        
+        let myRequest = NSMutableURLRequest(url: myURL!)
+        
+        let myTask = URLSession.shared.dataTask(with: myRequest as URLRequest) { data, response, error in
+            if error != nil {
+                print(error!)
+            } else {
+                if let unwrappedData = data {
+                    let dataNSString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)!
+                    print(dataNSString)
+                }
+            }
+        }
+        myTask.resume()
     }
-
+    
+    @IBAction func getWeatherTapped(_ sender: Any) {
+        /*
+        if let userInput = textField.text {
+            if let userURL = createURLFromInput(input: userInput) {
+                if let rawHTMLString = getWeatherData(url: userURL) {
+                    DispatchQueue.main.sync(execute: {
+                        if let webForecastString = getForecastFromData(rawData: rawHTMLString) {
+                            displayWeatherDescription(webForecast: webForecastString)
+                        } else {
+                            print("couldn't get data from web")
+                        }
+                    })
+                }
+            }
+        }
+ */
+        print("nothing happens when tapped")
+    }
+    
+    
+    @IBOutlet weak var weatherDescription: UILabel!
+    
+    
+    
+    /*
+    func createURLFromInput(input: String) -> URL? {
+        let newNSStringInput = NSString(string: input)
+        newNSStringInput.replacingOccurrences(of: " ", with: "-")
+        let newURLString = frontURLString + (newNSStringInput as String) as String + backURLString
+        if let returnURL = URL(string: newURLString) {
+            return returnURL
+        } else {
+            print ("error getting url from input")
+            return nil
+        }
+    }
+    
+    func getWeatherData(url: URL) -> String? {
+        let request = NSMutableURLRequest(url: url)
+        var dataNSString = NSString()
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                if let unwrappedData = data {
+                    dataNSString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)!
+                    let dataString = dataNSString as String
+                } else {
+                    print("could not get unwrapped data from URLSession dataTask")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getForecastFromData(rawData: String) -> String? {
+        
+    }
+    
+    func displayWeatherDescription(webForecast: String) {
+        weatherDescription.text = webForecast
+    }
+    */
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
