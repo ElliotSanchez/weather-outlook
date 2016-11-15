707af100eb0cff79a81839ec2f34e1a641f613f6
Elliot Sanchez <github@elliotsanchez.com>
2016-11-15 12:01:08 -0600

    Correctly pulls data from hardcoded URL and displays in label on load

--- weather-outlook/ViewController.swift
+++ weather-outlook/ViewController.swift
@@ -16,25 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let myURL = URL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
-        
-        let myRequest = NSMutableURLRequest(url: myURL!)
-        
-        let myTask = URLSession.shared.dataTask(with: myRequest as URLRequest) { data, response, error in
-            if error != nil {
-                print(error!)
-            } else {
-                if let unwrappedData = data {
-                    let dataNSString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)!
-                    print(dataNSString)
-                }
-            }
-        }
-        myTask.resume()
-    }
+
     
     @IBAction func getWeatherTapped(_ sender: Any) {
         /*
@@ -58,7 +40,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var weatherDescription: UILabel!
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var message = ""
+        
+        let myURL = URL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest")
+        
+        let myRequest = NSMutableURLRequest(url: myURL!)
+        
+        let myTask = URLSession.shared.dataTask(with: myRequest as URLRequest) { data, response, error in
+            if error != nil {
+                print(error!)
+            } else {
+                
+                if let unwrappedData = data {
+                    let dataNSString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    var separatorString = "3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">" // code directly before forecast
+                    
+                    if let contentArray = dataNSString?.components(separatedBy: separatorString) {
+                        
+                        if contentArray.count > 0 {
+                            separatorString = "</span>" // update value to reflect code immediately after forecast
+                            let newContentArray = contentArray[1].components(separatedBy: separatorString)
+                            if newContentArray.count > 0 {
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                print(message)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if message == "" {
+                message = "The weather there couldn't be found, please try again."
+            }
+            
+            DispatchQueue.main.async(execute: {
+                self.weatherDescription.text = message
+            })
+        }
+        myTask.resume()
+    }
     
     /*
     func createURLFromInput(input: String) -> URL? {
@@ -102,6 +125,8 @@ class ViewController: UIViewController {
         weatherDescription.text = webForecast
     }
     */
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
